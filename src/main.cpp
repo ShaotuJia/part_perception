5bd31530e162d8032a36b8562c43358a2088335e
ShaotuJia <shaotu.jia@hotmail.com>
2018-05-04 15:43:13 -0400

    disable function of detecting incorrect part on agv 1

--- src/main.cpp
+++ src/main.cpp
@@ -26,31 +26,34 @@ int main(int argc, char **argv) {
 	// instance of class Belt_Inventory
 	Belt_Inventory belt_inventory(node);
 
-	// subscribe topic /tf
+	// subscribe topic /tf and publish belf_inventory
 	ros::Subscriber sub = node.subscribe("/tf", 1000, &Belt_Inventory::build_belt_inventory, &belt_inventory);
 
 	// create ros server
 	// ros::ServiceServer server = node.advertiseService("add_two_ints", &Belt_Inventory::add, &belt_inventory);
 
+	// publish service that predicate the belt_inventory
 	ros::ServiceServer inventory_server = node.advertiseService("/ariac/query_belt_part", &Belt_Inventory::find_parts, &belt_inventory);
 
+	// disable the function of detecting incorrect part on agv 1
+
 	// initialize Incorrect_Pos_AGV class
-	Incorrect_Pos_AGV incorrect_pos_agv(node);
+	// Incorrect_Pos_AGV incorrect_pos_agv(node);
 
 	// subscribe /ariac/order to received order info
-	ros::Subscriber order_sub = node.subscribe("/ariac/orders", \
+	// ros::Subscriber order_sub = node.subscribe("/ariac/orders", \
 			1000, &Incorrect_Pos_AGV::order_callback, &incorrect_pos_agv);
 
 	// subscribe /tf to invoke all function to detect all incorrect parts on AGV_1
-	ros::Subscriber incorrect_part_sub = node.subscribe("/tf", 1000, &Incorrect_Pos_AGV::incorrect_part_call_back,\
+	// ros::Subscriber incorrect_part_sub = node.subscribe("/tf", 1000, &Incorrect_Pos_AGV::incorrect_part_call_back,\
 			&incorrect_pos_agv);
 
 	// obtain server data
-	ros::Subscriber server_data_sub = node.subscribe("/ariac/incorrect_parts_agv_1", 1000,\
+	// ros::Subscriber server_data_sub = node.subscribe("/ariac/incorrect_parts_agv_1", 1000,\
 			&Incorrect_Pos_AGV::server_data_call_back, &incorrect_pos_agv);
 
 	// create ros server to check part position on AGV_1
-	ros::ServiceServer part_pos_agv_server = node.advertiseService("/ariac/check_part_pos_agv_1", \
+	// ros::ServiceServer part_pos_agv_server = node.advertiseService("/ariac/check_part_pos_agv_1", \
 			&Incorrect_Pos_AGV::check_parts_pos, &incorrect_pos_agv);
 
 	// initialize Grip_Part class
